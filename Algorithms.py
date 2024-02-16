78154eeaf096e2cf99099f4851377b19e206dcf3
yonib98 <battat@campus.technion.ac.il>
2024-02-16 19:47:59 +0200

    Started implementing A-star algorithm, needs more work on expanded nodes and make sure it works

--- Algorithms.py
+++ Algorithms.py
@@ -9,16 +9,18 @@ from typing import List, Tuple
 import heapdict
 
 class Node:
-    def __init__(self, state: int, parent , cost: float, action: int, heuristic: float = None) -> None:
+    def __init__(self, state: int, parent ,cost: float, action: int, heuristic: float = None) -> None:
         self.state = state
         self.parent = parent
         self.cost = cost
         self.action = action
         self.heuristic = heuristic
-
+        
     def __eq__(self, other) -> bool:
         return other.state == self.state
     
+    def __hash__(self):
+        return hash(self.state)
     def __str__(self) -> str:
         return f"State: {self.state}, Parent: {self.parent}, Action: {self.action}, Cost: {self.cost}, Heuristic: {self.heuristic}"
     
@@ -87,14 +89,14 @@ class BFSAgent(Agent):
         # Graph bfs
         state = self.env.get_initial_state()
         node = Node(state,  None, 0, None) # Make a node.
-        if (self.env.is_final_state(state)):
+        if self.env.is_final_state(state):
             return [], 0, 0
         open = [node]
-        closed = []
+        closed = set()
         expanded_nodes = 0
         while len(open) > 0:
             node = open.pop(0)
-            closed.append(node.state)
+            closed.add(node.state)
             expanded_nodes += 1
             for action, (next_state, cost, terminated) in env.succ(node.state).items():
                 child = Node(next_state, node, cost, action)
@@ -109,10 +111,8 @@ class BFSAgent(Agent):
                     continue
                 if child.state not in closed and child not in open:
                     if self.env.is_final_state(child.state):
-                        print('Found a solution !')
                         return self._get_path_actions(child), self._get_path_total_cost(child), expanded_nodes
                     open.append(child)
-        # print('couldn\'t find a solution')
                     
 
                 
@@ -120,14 +120,72 @@ class BFSAgent(Agent):
         
 
 
-class WeightedAStarAgent():
+class WeightedAStarAgent(Agent):
     def __init__(self) -> None:
-        raise NotImplementedError
+        super().__init__()
 
+    def manhattan_distance(self, first_state, second_state):
+        first_row, first_col = self.env.to_row_col(first_state)
+        second_row, second_col = self.env.to_row_col(second_state)
+        return abs(first_row - second_row) + abs(first_col - second_col)
+
+    def hmsap(self, state) -> int:
+        distant_from_d1 = self.manhattan_distance(state, self.env.d1)
+        distant_from_d2 = self.manhattan_distance(state, self.env.d2)
+        distant_from_goals = [self.manhattan_distance(state, goal) for goal in self.env.get_goal_states()]
+        return min(distant_from_goals + [distant_from_d1, distant_from_d2])
+    
+    def f(self, node: Node, h_weight) -> float:
+        return ((1-h_weight) * node.cost + h_weight * node.heuristic, node.state[0])
+    
     def search(self, env: DragonBallEnv, h_weight) -> Tuple[List[int], float, int]:
-        raise NotImplementedError
+        self.env = env
+        self.env.reset()
+        state = self.env.get_initial_state()
+        node = Node(state,  parent=None, cost=0, action=None, heuristic=self.hmsap(state)) # Make a node.
 
+        open = heapdict.heapdict({node: self.f(node, h_weight)})
+        closed = {} # key: state, value: node
+        
+        expanded_nodes = 0
+        while len(open) > 0:
+            node, _ = open.popitem()
+            closed[node.state] = node
+            expanded_nodes += 1
+            if self.env.is_final_state(node.state):
+                return self._get_path_actions(node), node.cost, expanded_nodes
+        
+            for action, (next_state, edge_cost, terminated) in env.succ(node.state).items():
+                cost = node.cost + edge_cost # Cost==gvalue
+                child = Node(next_state, node, cost, action)
+                # Set child dragon ball's to true in 2 cases:
+                # a. Parent found the dragon ball; b. Child location is the dragon ball location.
+                child.state = child.state[0], \
+                              node.state[1] or child.state[0] == self.env.d1[0], \
+                              node.state[2] or child.state[0] == self.env.d2[0]
 
+                if terminated is True and self.env.is_final_state(child.state) is False:
+                    if child.cost == np.inf:
+                        continue    
+                        
+                child.heuristic=self.hmsap(child.state)
+                fval = self.f(child, h_weight)
+
+                if child.state not in closed and child not in open:
+                    open[child] = fval
+                elif child in open:
+                    old_fval = open[child]
+                    if fval < old_fval:
+                        open.pop(child) # Drop the old node.
+                        open[child] = fval  # Append the new node with the new fval.
+                else: 
+                    # Child in close
+                    old_child = closed[child.state]
+                    fval, old_fval = self.f(child, h_weight), self.f(old_child, h_weight)
+                    if fval < old_fval:
+                        open[child] = fval
+                        closed.pop(child.state)
+        print('didnt find solution')
 
 class AStarEpsilonAgent():
     def __init__(self) -> None:
